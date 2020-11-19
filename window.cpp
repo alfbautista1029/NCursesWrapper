ca70af9d38fcbea42b259b5048ce2bba1c33dcb6
Alfredo Bautista <56358692+alfbautista1029@users.noreply.github.com>
2020-11-18 23:40:44 -0500

    Add files via upload
    Fixed small bugs with displaying the window's border, added subwindow support.

--- window.cpp
+++ window.cpp
@@ -7,7 +7,7 @@
 
 #include "window.h"
 
-//*these cchar_t variables make up the normal default border on any window.
+//these cchar_t variables make up the normal default border on any window.
 cchar_t top_left = {A_NORMAL, L"\u250F"};
 cchar_t top_hori = {A_NORMAL, L"\u2501"};
 cchar_t top_right = {A_NORMAL, L"\u2513"};
@@ -23,29 +23,37 @@ cchar_t* norm_border[8] =
 	&left_vert, &right_vert, &top_hori, &bot_hori, &top_left, &top_right, &bot_left, &bot_right
 };
 
+
+/**********************************window class definitions BEGIN********************************************/
 window::window()
 {
 	_wbeg = {0, 0};
-	_wend = {LINES, COLS};
+	_wend = {LINES - 1, COLS - 1};
 	_cbeg = {1, 1};
-	_cend = {LINES - 2, LINES - 2};
+	_cend = {LINES - 2, COLS - 2};
 	_cpos = {1, 1};
 	_win = newwin(_wend.line_y, _wend.col_x, _wbeg.line_y, _wbeg.col_x);
 	set_border(norm_border);
-	_color_pair = 0;
+	_color_pair = 1;
 	_cpos.line_y = _cbeg.line_y;
 	_cpos.col_x = _cbeg.col_x;
 	wmove(_win, _cpos.line_y, _cpos.col_x);
-	
-	if(has_colors())
-	{
-		init_pair(1, COLOR_BLACK, COLOR_WHITE);
-		wbkgd(_win, COLOR_PAIR(1));
-	}		
+	init_pair(_color_pair, COLOR_WHITE, COLOR_BLACK);
+	wbkgd(_win, COLOR_PAIR(_color_pair));
 }
 
 window::window(cord begin, cord end)
 {
+	if(begin.line_y < 0)
+		begin.line_y = 0;
+	if(begin.col_x < 0)
+		begin.col_x = 0;
+	
+	if(end.line_y >= LINES)
+		end.line_y = LINES - 1;
+	if(end.col_x >= COLS)
+		end.col_x = COLS - 1;
+	
 	_wbeg = begin;
 	_wend = end;
 	_cbeg = {(begin.line_y + 1), (begin.col_x + 1)};
@@ -59,9 +67,20 @@ window::window(cord begin, cord end)
 	
 	if(has_colors())
 	{
-		init_pair(1, COLOR_BLACK, COLOR_WHITE);
+		init_pair(1, COLOR_WHITE, COLOR_BLACK);
 		wbkgd(_win, COLOR_PAIR(1));
 	}	
+}
+window::window(window &orig)
+{
+
+	 _win = orig._win;
+	_wbeg = orig._wbeg;
+	_wend = orig._wend;
+	_cbeg = orig._cbeg;
+	_cend = orig._cend;
+	_cpos = orig._cpos;
+	_color_pair = orig._color_pair;
 }
 
 WINDOW* window::get_win()
@@ -178,10 +197,203 @@ void window::win_clear()
 
 void window::set_color_pair(int new_pair)
 {
-	wbkgd(_win, COLOR_PAIR(new_pair));
+	_color_pair = new_pair;
+	wbkgd(_win, COLOR_PAIR(_color_pair));
 }
 
 int window::get_color_pair()
 {
 	return _color_pair;
 }
+
+void window::cur_move(cord new_pos)
+{
+	_cpos.line_y = new_pos.line_y;
+	_cpos.col_x = new_pos.col_x;
+	wmove(_win, _cpos.line_y, _cpos.col_x);
+}
+/**********************************window class definitions END********************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************************sub_window class definitions BEGIN************************************/
+sub_window::sub_window(window* par, cord begin, cord end) : parent(par)
+{
+	cord* par_beg = new cord;
+	cord* par_end = new cord;
+	parent->get_wbeg(*par_beg);
+	parent->get_wend(*par_end);
+	
+	if(begin.line_y <= par_beg->line_y)
+		s_wbeg.line_y = par_beg->line_y + 1;
+	if(begin.col_x <= par_beg->col_x)
+		s_wbeg.col_x = par_beg->col_x + 1;
+	
+	if(end.line_y >= par_end->line_y)
+		s_wend.line_y = par_end->line_y - 1;
+	if(end.col_x >= par_end->col_x)
+		s_wend.col_x = par_end->col_x - 1;
+	
+	s_wbeg = begin;
+	s_wend = end;
+	
+	s_cbeg = {(s_wbeg.line_y + 1), (s_wbeg.col_x + 1)};
+	s_cend = {(s_wend.line_y - 1), (s_wend.col_x - 1)};
+	
+	s_win = subwin(par->get_win(), s_wend.line_y, s_wend.col_x, s_wbeg.line_y, s_wbeg.col_x);
+	set_border(norm_border);
+	s_color_pair = 0;
+	s_cpos.line_y = s_cbeg.line_y;
+	s_cpos.col_x = s_cbeg.col_x;
+	
+	wmove(s_win, s_cpos.line_y, s_cpos.col_x);
+	
+	if(has_colors())
+	{
+		init_pair(1, COLOR_BLACK, COLOR_WHITE);
+		wbkgd(s_win, COLOR_PAIR(1));
+	}
+
+	delete par_beg;
+	delete par_end;
+}
+
+WINDOW* sub_window::get_win()
+{
+	return s_win;
+}
+
+void sub_window::get_wbeg(cord &co)
+{
+	co.line_y = s_wbeg.line_y;
+	co.col_x = s_wbeg.col_x;
+}
+
+void sub_window::get_wend(cord &co)
+{
+	co.line_y = s_wend.line_y;
+	co.col_x = s_wend.col_x;
+}
+
+void sub_window::get_cbeg(cord &co)
+{
+	co.line_y = s_cbeg.line_y;
+	co.col_x = s_cbeg.col_x;
+}
+
+void sub_window::set_cbeg(cord co)
+{
+	if(co.line_y <= s_wbeg.line_y)
+		co.line_y = s_wbeg.line_y + 1;
+	else if(co.line_y >= s_wend.line_y)
+		co.line_y = s_wend.line_y - 1;
+		
+	if(co.col_x <= s_wbeg.col_x)
+		co.col_x = s_wbeg.col_x + 1;
+	else if(co.col_x >= s_wend.col_x)
+		co.col_x = s_wend.col_x - 1;
+	
+	
+	s_cbeg.line_y = co.line_y;
+	s_cbeg.col_x = co.col_x;
+}
+
+void sub_window::get_cend(cord &co)
+{
+	co.line_y = s_cend.line_y;
+	co.col_x = s_cend.col_x;
+}
+
+void sub_window::set_cend(cord co)
+{
+	if(co.line_y <= s_wbeg.line_y)
+		co.line_y = s_wbeg.line_y + 1;
+	else if(co.line_y >= s_wend.line_y)
+		co.line_y = s_wend.line_y - 1;
+		
+	if(co.col_x <= s_wbeg.col_x)
+		co.col_x = s_wbeg.col_x + 1;
+	else if(co.col_x >= s_wend.col_x)
+		co.col_x = s_wend.col_x - 1;
+	
+	wmove(s_win, co.line_y, co.col_x);
+}
+
+void sub_window::get_cpos(cord &co)
+{
+	co.line_y = s_cpos.line_y;
+	co.col_x = s_cpos.col_x;
+}
+
+void sub_window::set_cpos(cord co)
+{
+	if(co.line_y <= s_wbeg.line_y)
+		co.line_y = s_wbeg.line_y + 1;
+	else if(co.line_y >= s_wend.line_y)
+		co.line_y = s_wend.line_y - 1;
+		
+	if(co.col_x <= s_wbeg.col_x)
+		co.col_x = s_wbeg.col_x + 1;
+	else if(co.col_x >= s_wend.col_x)
+		co.col_x = s_wend.col_x - 1;
+	
+	wmove(s_win, co.line_y, co.col_x);
+}
+
+cchar_t** sub_window::get_border()
+{
+	return s_border;
+}
+
+void sub_window::set_border(cchar_t* new_border[])
+{
+	for(int count = 0; count < 8; count++)
+	{
+		s_border[count] = new_border[count];
+	}
+}
+
+void sub_window::draw_border()
+{
+	wborder_set(s_win, s_border[0], s_border[1], s_border[2], s_border[3], s_border[4], s_border[5], s_border[6], s_border[7]);
+}
+
+void sub_window::win_refresh()
+{
+	refresh();
+	wrefresh(parent->get_win());
+	wrefresh(s_win);
+}
+
+void sub_window::win_clear()
+{
+	wclear(s_win);
+	win_refresh();
+}
+
+void sub_window::set_color_pair(int new_pair)
+{
+	wbkgd(s_win, COLOR_PAIR(new_pair));
+}
+
+int sub_window::get_color_pair()
+{
+	return s_color_pair;
+}
+
+void sub_window::cur_move(cord new_pos)
+{
+	s_cpos.line_y = new_pos.line_y;
+	s_cpos.col_x = new_pos.col_x;
+	wmove(s_win, s_cpos.line_y, s_cpos.col_x);
+}
+/**********************************sub_window class definitions END**************************************/
--- window.h
+++ window.h
@@ -67,55 +67,160 @@ class window
 		
 		//constructor that sets up a window based on the beginning and ending coordinates for the window 
 		window(cord, cord);
+	
+		//copy constructor
+		window(window&);
 		
 		//returns the window's WINDOW pointer 
-		WINDOW* get_win();
+		virtual WINDOW* get_win();
 		
 		//returns the window's beginning cooridinate
-		void get_wbeg(cord &);
+		virtual void get_wbeg(cord &);
 		
 		//returns the windows ending cooridinate
-		void get_wend(cord &);
+		virtual void get_wend(cord &);
 		
 		//returns the beginning cooridinate limit for the cursor
-		void get_cbeg(cord &);
+		virtual void get_cbeg(cord &);
 		
 		//sets the beginning cooridinate limit for the cursor
-		void set_cbeg(cord);
+		virtual void set_cbeg(cord);
 		
 		//returns the ending cooridinate limit for the cursor
-		void get_cend(cord &);
+		virtual void get_cend(cord &);
 		
 		//sets the beginning cooridinate limit for the cursor
-		void set_cend(cord);	
+		virtual void set_cend(cord);	
 		
 		//returns the current cursor position
-		void get_cpos(cord &);
+		virtual void get_cpos(cord &);
 		
 		//sets the beginning cooridinate limit for the cursor
-		void set_cpos(cord);
+		virtual void set_cpos(cord);
 		
 		//returns the current border array
-		cchar_t** get_border();
+		virtual cchar_t** get_border();
 			
 		//sets a new border configuration - follows the same layout as the ncurses defined wborder_set(...) 
-		void set_border(cchar_t* new_border[]);
+		virtual void set_border(cchar_t** new_border);
 		
 		//draws the border around the window using the characters stored in the member _border array
-		void draw_border();
+		virtual void draw_border();
 		
 		//refreshes the window and the ncurses-defined standard screen (stdscr) window 
-		void win_refresh();
+		virtual void win_refresh();
 	
 		//clears the window (erases the border and any text in the window
-		void win_clear();
+		virtual void win_clear();
 	
 		//sets the window's current color pair
-		void set_color_pair(int new_pair);
+		virtual void set_color_pair(int new_pair);
 		
 		//returns the window's current color pair
-		int get_color_pair();
+		virtual int get_color_pair();
+	
+		//move the window's cursor to the specified 
+		virtual void cur_move(cord);
 };
 
+class sub_window : public window
+{
+	private:
+		window* parent;
+		//main subwindow pointer that is linked with a parent window 
+		WINDOW* s_win;
+		
+		/*
+			the starting and ending cooridinates of the subwindow
+			
+			s_wbeg details where the window's origin will be.
+			s_wend details the last point of the window as well as the window's size relative to the parent window
+		 */
+
+		cord s_wbeg;
+		cord s_wend;
+		
+		/* 
+		   the starting and ending cooridinate positions for the subwindow's cursor.
+		   
+		   these coordinates set limits on where the cursor is able to print characters
+		   to avoid having the cursor overwrite the subwindow's border
+		 */
+		cord s_cbeg;
+		cord s_cend;
+		
+		// the current cooridinate position of the cursor 
+		cord s_cpos;
+
+		// an array of type cchar_t (for wide character support) that holds the characters for the
+		// window's border
+		cchar_t* s_border[8];
+
+		//this integer will hold the window's current color pair (color text on color background)
+		int s_color_pair;
+	
+		//the default constructor is privated, because it wouldn't make sense to create a subwindow
+		//without having a parent window 
+		sub_window();
+	
+	public:
+	
+	//constructor that takes *in a parent window object to link to
+	sub_window(window* par);
+	
+	//constructor that takes in a parent window as well as a beginning and ending coordinate
+	sub_window(window* par, cord s_wbeg, cord s_wend);
+	
+	//returns the sub window's WINDOW pointer
+	WINDOW* get_win();
+	
+	//returns the sub_window's beginning cooridinate
+	void get_wbeg(cord &);
+	
+	//returns the sub_window's ending cooridinate
+	void get_wend(cord &);
+	
+	//returns the beginning cooridinate limit for the sub_window's cursor
+	void get_cbeg(cord &);
+	
+	//sets the beginning cooridinate limit for the sub_window's cursor
+	void set_cbeg(cord);
+	
+	//returns the ending cooridinate limit for the sub_window's cursor
+	void get_cend(cord &);
+	
+	//sets the beginning cooridinate limit for sub_window's the cursor
+	void set_cend(cord);	
+	
+	//returns the current sub_window's cursor position
+	void get_cpos(cord &);
+	
+	//sets the beginning cooridinate limit for the sub_window's cursor
+	void set_cpos(cord);
+	
+	//returns the current sub_window's border array
+	cchar_t** get_border();
+		
+	//sets a new border configuration - follows the same layout as the ncurses defined wborder_set(...) 
+	void set_border(cchar_t* new_border[]);
+	
+	//draws the border around the sub_window using the characters stored in the member _border array
+	void draw_border();
+	
+	//refreshes the sub_window and the ncurses-defined standard screen (stdscr) window 
+	void win_refresh();
+
+	//clears the sub_window (erases the border and any text in the window
+	void win_clear();
+
+	//sets the sub_window's current color pair
+	void set_color_pair(int new_pair);
+	
+	//returns the sub_window's current color pair
+	int get_color_pair();
+
+	//move the sub_window's cursor to the specified 
+	void cur_move(cord);
+};
 
 #endif /* window_h */
